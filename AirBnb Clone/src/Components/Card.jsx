ca88b06d4b0cdb5336348d4de3097445ace07a4d
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-05 09:40:33 +0200

    add items with props to make sure that everything looks the same

--- AirBnb Clone/src/Components/Card.jsx
+++ AirBnb Clone/src/Components/Card.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Card(props){
     let textBadge
 
-    if(props.openSpots === 0){
+    if(props.item.openSpots === 0){
         textBadge = "SOLD OUT"
-    }else if(props.location === "Online"){
+    }else if(props.item.location === "Online"){
         textBadge = "ONLINE"
     }else{
         textBadge=" SPACE AVAILABLE"
@@ -13,14 +13,14 @@ export default function Card(props){
     return(
         <div className="card">
             {textBadge && <div className="card-badge">{textBadge}</div>}
-            <img src={`../Images/${props.img}`} className="card-image"/>
+            <img src={`../Images/${props.item.coverImg}`} className="card-image"/>
             <div className="card-stats">
                 <img src="../Images/star.png" className="card-star"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p className="card-title">{props.title}</p>
+            <p className="card-title">{props.item.title}</p>
             <p className="card-price">
                 <span className="bold">from {props.price}</span> / person
             </p>
