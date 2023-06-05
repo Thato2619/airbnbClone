8e1261c3232932fd33167706092e940dcfe6aca8
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-05 05:51:38 +0200

    add new photo-grid image from scrimab

--- AirBnb Clone/src/Components/Hero.jsx
+++ AirBnb Clone/src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero(){
     return(
         <section className="hero">
-            <img src="../Images/photo_grid.png" className="hero-photo"/>
+            <img src="../Images/photo-grid.png" className="hero-photo"/>
             <h1 className="hero-header">Online Experience</h1>
             <p className="hero-text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
