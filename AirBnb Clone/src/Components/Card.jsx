534512317cc2b4a3785bd6c86817500fe6fac81c
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-05 09:09:16 +0200

    create logic for the openSpots and style a badge for each output expected

--- AirBnb Clone/src/Components/Card.jsx
+++ AirBnb Clone/src/Components/Card.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
 export default function Card(props){
+    let textBadge
+
+    if(props.openSpots === 0){
+        textBadge = "SOLD OUT"
+    }else if(props.location === "Online"){
+        textBadge = "ONLINE"
+    }else{
+        textBadge=" SPACE AVAILABLE"
+    }
     return(
         <div className="card">
+            {textBadge && <div className="card-badge">{textBadge}</div>}
             <img src={`../Images/${props.img}`} className="card-image"/>
             <div className="card-stats">
                 <img src="../Images/star.png" className="card-star"/>
