4a9396172e3e59392932a94da0590d95e4e76e7b
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-05 05:21:52 +0200

    import all files in component folder in app.js

--- AirBnb Clone/src/App.jsx
+++ AirBnb Clone/src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import './App.css'
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Card from './Components/Card';
+import './App.css';
 
 function App() {
  
 
   return (
-    <>
+   <div>
       <Navbar />
-    </>
+      <Hero />
+      <Card />
+   </div>
   )
 }
 
--- AirBnb Clone/src/Components/Hero.jsx
+++ AirBnb Clone/src/Components/Hero.jsx
@@ -3,9 +3,10 @@ import React from "react";
 export default function Hero(){
     return(
         <section>
-            <img />
-            <h1></h1>
-            <p></p>
+            <img src="../Images/photo-grid.png" className="hero-photo"/>
+            <h1 className="hero-header">Online Experience</h1>
+            <p className="hero-text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
         </section>
     )
 }
