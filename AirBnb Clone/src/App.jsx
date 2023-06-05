73582c4dd7b559b3334b22d7dcce1b0dc22115d3
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-05 05:32:51 +0200

    remove Navbar component, just testing purposes

--- AirBnb Clone/src/App.jsx
+++ AirBnb Clone/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Card from './Components/Card';
 import './App.css';
@@ -9,7 +8,6 @@ function App() {
 
   return (
    <div>
-      <Navbar />
       <Hero />
       <Card />
    </div>
